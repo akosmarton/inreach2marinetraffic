bb7b03753bd1f44f222cccc2c5d130532103f937
Akos Marton <akos.marton@gmail.com>
2020-07-28 21:50:58 +0300

    Fix email body format

--- marinetraffic.go
+++ marinetraffic.go
@@ -17,11 +17,11 @@ type SelfReport struct {
 const MARINETRAFFIC_EMAIL = "report@marinetraffic.com"
 
 func (r *SelfReport) ToString() string {
-	body := "________________\r\n"
-	body += fmt.Sprintf("MMSI=%s\r\n", r.Mmsi)
-	body += fmt.Sprintf("LAT=%.5f\r\n", r.Lat)
-	body += fmt.Sprintf("LON=%.5f\r\n", r.Lon)
-	body += fmt.Sprintf("SPEED=%.1f\r\n", r.Speed)
+	body := "________________\n"
+	body += fmt.Sprintf("MMSI=%s\n", r.Mmsi)
+	body += fmt.Sprintf("LAT=%.5f\n", r.Lat)
+	body += fmt.Sprintf("LON=%.5f\n", r.Lon)
+	body += fmt.Sprintf("SPEED=%.1f\n", r.Speed)
 	body += fmt.Sprintf("COURSE=%.0f\n", r.Course)
 	body += fmt.Sprintf("TIMESTAMP=%s\n", r.Timestamp)
 	body += "________________\n"
